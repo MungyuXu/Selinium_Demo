6fa2ffeff52286427bb8da3c0304a58cce963258
353991880@qq.com <X15557400848my>
2019-09-18 17:08:20 +0800

    WebDriverDemo和pom.xml中加入log4j日志

--- src/main/java/TestDemo/WebDriverDemo.java
+++ src/main/java/TestDemo/WebDriverDemo.java
@@ -1,14 +1,22 @@
 package TestDemo;
 
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.logging.Logger;
 
 /**
  * 打开浏览器进入网址，设置浏览器宽高
  */
 public class WebDriverDemo {
     public static void main(String[] args) throws InterruptedException {
+
+        Logger logger=Logger.getLogger("baidu");
+//        PropertyConfigurator.configure(".\\Log4j.properties");
+
+
         //指定浏览器驱动路径
         System.setProperty("webdriver.chrome.driver","C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\Application\\chromedriver.exe");
        //初始化浏览器命名为driver
@@ -22,5 +30,6 @@ public class WebDriverDemo {
         driver.manage().window().setSize(new Dimension(480,800));
         //关闭浏览器
         driver.quit();
+        logger.info("退出浏览器");
     }
 }
